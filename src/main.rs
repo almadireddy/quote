688061a53f3f1b620e5f5c194674416664cb977e
Al Madireddy <al.madireddy@gmail.com>
2021-03-14 15:47:10 -0500

    added news listings

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,15 @@ use std::env;
 use textplots::{Chart, Plot, Shape};
 use chrono::{Duration, Utc};
 
-fn main() {
+async fn run() {
     let args: Vec<String> = env::args().collect();
     let ticker = &args[1];
 
     let last_month = Utc::now() - Duration::days(45);
     let provider = yahoo::YahooConnector::new();
-    let response = block_on(provider.get_quote_history(ticker, last_month, Utc::now())).unwrap();
+    let response = provider.get_quote_history(ticker, last_month, Utc::now()).await.unwrap();
+    let meta_response = provider.search_ticker(ticker).await.unwrap();
+    let meta = &meta_response.quotes[0];
 
     let quote = response.last_quote().unwrap();
     let quotes = response.quotes().unwrap();
@@ -20,10 +22,23 @@ fn main() {
         quotes_formatted.push((pos as f32, e.close as f32));
     }
 
-    Chart::new(180, 60, 0.0, quotes.len() as f32).lineplot(&Shape::Lines(quotes_formatted.as_slice())).nice();
+    Chart::new(100, 60, 0.0, quotes.len() as f32).lineplot(&Shape::Lines(quotes_formatted.as_slice())).nice();
 
-    println!("{} quote", ticker.to_uppercase());
+
+    println!("${} | {}", ticker.to_uppercase(), meta.long_name);
     println!("----------");
     println!("Open: {:.2}, High: {:.2}, Low: {:.2}", quote.open, quote.high, quote.low);
     println!("Volume: {:.2}", quote.volume);
+    println!("__________");
+    
+    for i in 0..5 {
+        if i > meta_response.news.len() {
+            break;
+        }
+        println!("{}", meta_response.news[i].title);
+    }
+}
+
+fn main() {
+    block_on(run());
 }
